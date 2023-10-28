d2e6bcc8504e89b91729bdbc0acd762e194bf352
Cardiac <83916775+Cardiacman13@users.noreply.github.com>
2023-10-28 22:07:25 +0200

    Update reboot_timer.sh

--- src/reboot_timer.sh
+++ src/reboot_timer.sh
@@ -4,11 +4,11 @@
 function reboot_timer() {
     local timer=$1
 
-    echo -e "${RED}The script has completed, and the system needs to restart${RESET}: Press ${GREEN}Enter${RESET} to reboot or ${GREEN}Ctrl+C${RESET} to cancel."
+    echo -e "${GREEN}The script has completed, and the system needs to restart${RESET}: Press ${GREEN}Enter${RESET} to reboot or ${GREEN}Ctrl+C${RESET} to cancel."
     read -p "" choice
     if [[ "${choice}" == "" ]]; then
         while [[ ${timer} -gt 0 ]]; do
-            echo -e "${RED}Restarting in ${timer} seconds...${RESET}"
+            echo -e "${GREEN}Restarting in ${timer} seconds...${RESET}"
             sleep 1
             ((timer--))
         done
